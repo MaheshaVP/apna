148d77ed876d3093f25714975f6947f29354961c
Mahesha V P <maheshavp12@gmail.com>
2024-12-24 23:54:45 +0530

    javascript

--- javascript/js2/app.js
+++ javascript/js2/app.js
@@ -53,6 +53,7 @@ if(str[0]==='a' && str.length>=3) {
     console.log("try another word");
 }
 
+
 let num=12;
 if((num%3 ===0)&&((num+1==15)||(num-1==11))){
     console.log("safe");
--- javascript/js4/app.js
+++ javascript/js4/app.js
@@ -32,10 +32,92 @@ for(let i=6;i<=60;i=i+6){
     console.log(i);
 }
 
-let n = prompt("write your number");
-n = parseInt(n);
 
-for(let i=n;i<=n*10;i=i+n){
-    console.log(i);
+//multiplication of 5
+// let n = prompt("write your number");
+// n = parseInt(n);
+
+// for(let i=n;i<=n*10;i=i+n){
+//     console.log(i);
+// }
+
+//nested loops
+for (let i=1;i<=5;i++){
+    console.log('outer loop ${i}');
+    for(let j=1;j<=4;j++){
+        console.log(j);
+    }
 }
 
+//while loop
+console.log("while loop");
+let i=1;
+while(i<=6){
+    console.log(i);
+    i++;
+}
+
+//favourate movie
+// let favMovie = "Avator";
+// let guess = prompt("enter your favourate movie");
+// while((guess != favMovie)&&(guess!='quit')) {
+//     console.log("correct");
+//     guess = prompt("wrong guess. please try again");
+// }
+
+// if(guess == favMovie) {
+//     console.log("congrats");
+// }else{
+//     console.log("quit");
+// }
+
+//break
+console.log("break");
+let f=1;
+while(f<=5){
+    if(f==3){
+        break;
+    }
+    console.log(f);
+    f++;
+}
+console.log("we used break at 3");
+
+//loops with array
+console.log("loops with array");
+let fruits = ["mango","apple","banana","litchi","orange"];
+fruits.push("pineapple");
+for(let i=0;i<=fruits.length;i++){
+    console.log(i,fruits[i]);
+}
+
+//reverse
+console.log("reverse");
+let fruits2 = ["mango","apple","banana","litchi","orange"];
+fruits2.push("pineapple");
+for(let i=fruits2.length-1;i>=0;i--){
+    console.log(i,fruits2[i]);
+}
+
+// //loops with nested arrays
+// let hero = [
+//     ["ironman","spyderman","thor"],
+//     ["superman","wonder man","flash"]
+// ]
+
+// for (let i=0;i<length.hero;i++){
+//     console.log(i,hero[i],hero[i].length);
+//     for(let j=0;j<hero[i].length;j++){
+//      console.log("j=${j},${hero[i][j]}")
+//      }
+// }
+
+//student
+let student = [["aman",95],["shradda",94.4],["karan",100]];
+
+for (let i=0;i<student.length;i++){
+    console.log("info of students #${i}");
+    for (let j=0;j<student[i].length;j++){
+        console.log(student[i][j]);
+        }
+}
