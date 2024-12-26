a1bf8311792cc86a52e79fba86187c3c6d379fb3
Mahesha V P <maheshavp12@gmail.com>
2024-12-27 00:27:44 +0530

    javascript

--- javascript/js5/object.js
+++ javascript/js5/object.js
@@ -33,3 +33,41 @@ const obj = {
     undefined: "e"
 };
 
+//obejects of objects
+const classInfo = {
+    mahesh:{
+        grade1 : "A",
+        city1 : "Tumkur"
+    },
+    kalyan:{
+        grade1 : "A+",
+        city1 : "Chitradurga"
+    },
+    shiva:{
+        grade1 : "o",
+        city1 : "Srinivasapura"
+    },
+};
+
+const classInfo2 = [
+    {
+        name: "mahesh",
+        grade1 : "A",
+        city1 : "Tumkur"
+    },
+    {
+        name: "kalyan",
+        grade1 : "A+",
+        city1 : "Chitradurga"
+    },
+    {
+        name: "shiva",
+        grade1 : "o",
+        city1 : "Srinivasapura"
+    },
+];
+
+
+//random numbers
+
+
