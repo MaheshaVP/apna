36a93c84a2ae13660f5ad4e510a5ee7add1b286f
Mahesha V P <maheshavp12@gmail.com>
2024-12-26 23:13:36 +0530

    javascript

---
+++ javascript/js5/object.js
@@ -0,0 +1,35 @@
+//object literals
+const student = {
+    name : "Mahesh",
+    age : 23,
+    marks : 94.4 ,
+    city: "Delhi"
+};
+
+
+
+const item = {
+    price : 100.99,
+    discount : 50,
+    colors : ["red","pink"]
+};
+
+const post = {
+    username: "@Mahesh",
+    content: "This is my #firstpost",
+    likes: 150,
+    reposts: 5,
+    tags: ["@college","@BCA"]
+};
+
+// //get values
+// post["tags"];
+
+const obj = {
+    1: "a",
+    2: "b",
+    true: "c",
+    null: "d",
+    undefined: "e"
+};
+
