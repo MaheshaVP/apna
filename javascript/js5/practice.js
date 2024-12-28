e61e8ef2dd1f44dba0959b7f59b3afbf6aad88d1
Mahesha V P <maheshavp12@gmail.com>
2024-12-28 18:24:30 +0530

    javascript

---
+++ javascript/js5/practice.js
@@ -0,0 +1,22 @@
+//Dice roll
+let Dice = (Math.floor(Math.random() * 6)+1);
+console.log(Dice);
+
+//car properties
+let car = {
+    name : "Breeza",
+    model: 2021,
+    console: "white"
+}
+
+console.log(car);
+
+//Details
+let person = {
+    name : "Rakii",
+    age : 32,
+    city : "New Yark",
+}
+person.city = "Banglore";
+
+console.log(person);
