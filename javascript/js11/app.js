372698b45120c96a959fd284542d9f630c2e86d4
Mahesha V P <maheshavp12@gmail.com>
2025-01-14 23:13:41 +0530

    js

--- javascript/js11/app.js
+++ javascript/js11/app.js
@@ -27,3 +27,18 @@ function three() {
     console.log(ans);
 }
 three();
+
+//Single threaded language
+let a = 25;
+console.log(a);
+let b = 10;
+console.log(b);
+console.log(a+b);
+
+
+setTimeout(() =>  {
+    console.log("apna college");
+},2000);
+
+console.log("hello...");
+
