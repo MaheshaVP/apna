42e0765031728b35bdd197a365e5910760159e38
Mahesha V P <maheshavp12@gmail.com>
2025-01-16 23:36:49 +0530

    js

--- javascript/js11/app.js
+++ javascript/js11/app.js
@@ -36,9 +36,46 @@ console.log(b);
 console.log(a+b);
 
 
-setTimeout(() =>  {
-    console.log("apna college");
-},2000);
+// setTimeout(() =>  {
+//     console.log("apna college");
+// },2000);
 
-console.log("hello...");
+// console.log("hello...");
+
+//callback hell
+
+h1 = document.querySelector("h1");
+
+function changeColor (color1,delay,nextColorChange) {
+    setTimeout (() => {
+        h1.style.color = color1;
+        if(nextColorChange) nextColorChange();
+    },delay);
+}
+
+changeColor("red",1000, ()=>{
+    changeColor("orange",1000, () => {
+        changeColor("yellow",1000, () => {
+
+        });
+    });
+});
+
+// changeColor("red",1000);
+// changeColor("green",2000);
+// changeColor("orange",3000);
+
+function savetoDb(data) {
+    let internetSpeed = Math.floor(Math.random() * 10 ) + 1;
+    if(internetSpeed > 4) {
+        console.log("your data was saved", data);
+    }else{
+        console.log("weak connection");
+    }
+}
+
+savetoDb("Apna College");
+
+
+
 
