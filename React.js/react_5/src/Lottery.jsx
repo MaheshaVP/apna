2b7f47cc61e734fa07784538d12c415d88155a32
Mahesha V P <maheshavp12@gmail.com>
2025-01-31 00:05:10 +0530

    rect

---
+++ React.js/react_5/src/Lottery.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import './Lottery.css'
+import {genTickets,sum} from './helper';
+
+
+export default function Lottery() {
+    let[ticket,setTicket] = useState(genTickets(3));
+    let isWinning = sum(ticket) === 15;
+
+    return (
+
+        <div>
+            <h1>Lottery ticket</h1>
+            <div className="ticket">
+                <span>{ticket[0]}</span>
+                <span>{ticket[1]}</span>
+                <span>{ticket[2]}</span>
+            </div>
+            <h3>{isWinning && "Congraduations you won"}</h3>
+        </div>
+    )
+}
