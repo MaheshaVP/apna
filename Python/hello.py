9c6fb54c4c54b66c4487eb949cc5abbe76d8d9e4
Mahesha V P <maheshavp12@gmail.com>
2025-04-27 12:30:57 +0530

    python

---
+++ Python/hello.py
@@ -0,0 +1,51 @@
+import sys
+
+print("python first program..")
+
+#to know python version
+print(sys.version)
+
+
+#indentation 
+if(5>2):
+    print("five is greater than two")
+
+#this is comment
+print("This is comment")
+
+
+#variables
+x = 5
+y = "Hello world"
+
+print(x)
+print(y)
+
+print("Hello world") #this is the side line
+
+#print("Hello world")
+print("cheers,mate")
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#multiline comment
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Multiline comments  ") 
+
+#new line
+""""
+multiline comment
+"""
+print("Hello world \n hello guru")
+
+if 5>2:
+    print("5 is greater")
+if 5<6:
+    print("5 is smaller")
