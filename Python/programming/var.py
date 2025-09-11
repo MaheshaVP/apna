0c1dc21826666d008094ca9f4f2b24b18674ea46
Mahesha V P <maheshavp12@gmail.com>
2025-09-11 16:17:13 +0530

    programming

---
+++ Python/programming/var.py
@@ -0,0 +1,38 @@
+favAnimal = "Tiger"
+print("I like "+ favAnimal)
+
+a = "Mahesh"
+print("My name is"+ " " + a )
+
+c = "mahi"
+d = "My name is"
+print(d+" "+c)
+
+
+e=3
+f=5
+g=e+f
+print("sum of e and f is " + str(g))
+print("sum of e and f is " ,g)
+print(f"sum of e and f is {g}")
+
+i = 3
+j = 5
+print(i+j)  
+
+b = 224
+b = b+1
+print(b,"\n")
+
+
+temperature = 25
+print("Temperature is "+ str(temperature) + "C")
+
+if temperature > 18:
+    print("Weather is warm")
+else:
+    print("Weather is cold")
+
+for i in range(10,0,-1):
+    print(i)
+print("Complete")
